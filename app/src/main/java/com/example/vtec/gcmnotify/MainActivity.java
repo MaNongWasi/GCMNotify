c5ccd99059f085a2cf344df12ba49d098b44920e
MaNongWasi <jingjingwang0051@gmail.com>
2017-05-15 12:40:26 +0200

    move aws function to AWS Manager

--- app/src/main/java/com/example/vtec/gcmnotify/MainActivity.java
+++ app/src/main/java/com/example/vtec/gcmnotify/MainActivity.java
@@ -5,8 +5,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
-import android.os.AsyncTask;
-import android.preference.PreferenceManager;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,16 +12,8 @@ import android.text.TextUtils;
 import android.util.Log;
 import android.widget.Toast;
 
-import com.amazonaws.services.sns.model.CreatePlatformEndpointRequest;
-import com.amazonaws.services.sns.model.CreatePlatformEndpointResult;
-import com.amazonaws.services.sns.model.DeleteEndpointRequest;
-import com.amazonaws.services.sns.model.SubscribeRequest;
-import com.amazonaws.services.sns.model.SubscribeResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
-import com.google.android.gms.gcm.GoogleCloudMessaging;
-
-import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
     private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
@@ -48,20 +38,22 @@ public class MainActivity extends AppCompatActivity {
                 } else {
                     System.out.println("Succeeded push registration");
                     Toast.makeText(MainActivity.this, "Succeeded push registration", Toast.LENGTH_LONG).show();
-                    if (token != null){
+                    if (token != null) {
                         connectAWS(token);
                     }
                 }
             }
         };
 
-        if (!checkPlayServices()) {
-            Toast.makeText(MainActivity.this, "This application needs Google Play services", Toast.LENGTH_LONG).show();
-            return;
-        }else{
-            SharedPreferences sharedPreferences = getSharedPreferences("my_prefs", Context.MODE_PRIVATE);
-            Boolean register = sharedPreferences.getBoolean("register", false);
-            if (!register){
+        SharedPreferences sharedPreferences = getSharedPreferences("my_prefs", Context.MODE_PRIVATE);
+        Boolean register = sharedPreferences.getBoolean("register", false);
+
+        if (!register) {
+            if (!checkPlayServices()) {
+                Toast.makeText(MainActivity.this, "This application needs Google Play services", Toast.LENGTH_LONG).show();
+                return;
+            } else {
+
                 Intent intent = new Intent(MainActivity.this, RegistrationIntentService.class);
                 System.out.println("start service");
                 startService(intent);
@@ -101,147 +93,12 @@ public class MainActivity extends AppCompatActivity {
 
     public void connectAWS(String token) {
         String email = "email";
-        String application = "platform application";
+        String application = "arn of application flatform";
         if (!TextUtils.isEmpty(token)) {
             System.out.println("connectaws");
-            new AWSCreateEndpointTask(this).execute(application, token, email);
+            new AWSManager.AWSCreateEndpointTask(this).execute(application, token, email);
 
         }
     }
-
-    public class AWSCreateEndpointTask extends AsyncTask<String, Void, CreatePlatformEndpointResult> {
-
-        Context context;
-
-        public AWSCreateEndpointTask(Context context) {
-
-            super();
-
-            this.context = context;
-
-        }
-
-        @Override
-
-        protected CreatePlatformEndpointResult doInBackground(String[] params) {
-
-            if (params.length < 3) {
-
-                return null;
-
-            }
-
-            String arn = params[0];
-
-            String gcmToken = params[1];
-
-            String userData = params[2];
-
-            try {
-                CreatePlatformEndpointRequest request = new CreatePlatformEndpointRequest();
-
-                request.setCustomUserData(userData);
-
-                request.setToken(gcmToken);
-
-                request.setPlatformApplicationArn(arn);
-
-                return AWSManager.getSNSClient().createPlatformEndpoint(request);
-
-            } catch (Exception ex) {
-
-                return null;
-
-            }
-
-        }
-
-        @Override
-
-        protected void onPostExecute(CreatePlatformEndpointResult result) {
-            System.out.println("result " + result);
-
-            if (result != null) {
-                String endpointArn = result.getEndpointArn();
-
-                System.out.println("endpoint " + endpointArn);
-                String topArn = "my topArn";
-                System.out.println("subscrip");
-                new SubscribeTopicTask(MainActivity.this).execute(topArn, "application", endpointArn);
-            }
-
-        }
-
-    }
-
-    public class SubscribeTopicTask extends AsyncTask<String, Void, SubscribeResult> {
-
-        Context context;
-
-        public SubscribeTopicTask(Context context) {
-
-            super();
-
-            this.context = context;
-
-        }
-
-        @Override
-
-        protected  SubscribeResult doInBackground(String[] params) {
-
-            if (params.length < 3) {
-
-                return null;
-
-            }
-
-            String topArn = params[0];
-
-            String protocol = params[1];
-
-            String endPoint = params[2];
-
-            System.out.println("subscribe result");
-
-            try {
-                SubscribeRequest request = new SubscribeRequest();
-
-                request.setTopicArn(topArn);
-
-                request.setProtocol(protocol);
-
-                request.setEndpoint(endPoint);
-
-
-                return AWSManager.getSNSClient().subscribe(request);
-
-            } catch (Exception ex) {
-
-                return null;
-
-            }
-
-        }
-
-        @Override
-
-        protected void onPostExecute(SubscribeResult result) {
-            System.out.println("result " + result);
-
-            if (result != null) {
-                SharedPreferences prefs = context.getSharedPreferences("my_prefs", Context.MODE_PRIVATE);
-
-                String subscriptionArn = result.getSubscriptionArn();
-
-                prefs.edit().putBoolean("register", true).commit(); //apply();
-                System.out.println("subscriptionArn " + subscriptionArn);
-
-            }
-
-        }
-
-    }
-
 
 }
